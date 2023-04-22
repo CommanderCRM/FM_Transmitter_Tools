76f7a0e26d644dca4c488c1525f678a5b496d4dd
Ilya Krivoshein <htctomsk634@gmail.com>
2023-04-22 11:17:57 +0700

    Update music.py
    add docstrings, removed global var

--- music.py
+++ music.py
@@ -2,56 +2,56 @@ import os
 import uuid
 from mutagen.id3 import ID3, ID3NoHeaderError
 
-PATH = 'F:\\'
-FILE_COUNT = 0
-
 
 def hello() -> None:
+    '''Greeting the user and prompting them to choose a function.'''
     print('Enter 1 for random rename, 2 for tag removing, 3 for file recreation, 4 for everything')
     inp = int(input())
     if inp == 1:
-        random_rename()
+        random_rename(PATH, FILE_COUNT)
     elif inp == 2:
-        remove_tags()
+        remove_tags(PATH, FILE_COUNT)
     elif inp == 3:
-        file_recreation()
+        file_recreation(PATH, FILE_COUNT)
     elif inp == 4:
-        random_rename()
-        remove_tags()
-        file_recreation()
+        random_rename(PATH, FILE_COUNT)
+        remove_tags(PATH, FILE_COUNT)
+        file_recreation(PATH, FILE_COUNT)
     else:
         hello()
 
 
-def count_files() -> None:
-    global FILE_COUNT
-    for files in os.walk(PATH):
+def count_files(path: str) -> int:
+    '''Counting files in folder for future global use'''
+    file_count = 0
+    for files in os.walk(path):
         for file in files:
             if file.endswith('.mp3'):
-                FILE_COUNT += 1
+                file_count += 1
+    return file_count
 
 
-def random_rename() -> None:
-    global FILE_COUNT
+def random_rename(path: str, file_count: int) -> None:
+    '''Renaming files using UUID4'''
     curr_count = 0
-    for file in os.listdir(PATH):
+    for file in os.listdir(path):
         if file.endswith('.mp3'):
             curr_count += 1
             file_extension = os.path.splitext(file)[1]
             new_filename = str(uuid.uuid4()) + file_extension
-            print(f'Renaming file {curr_count} out of {FILE_COUNT}')
-            os.rename(os.path.join(PATH, file),
-                      os.path.join(PATH, new_filename))
+            print(f'Renaming file {curr_count} out of {file_count}')
+            os.rename(os.path.join(path, file),
+                      os.path.join(path, new_filename))
 
 
-def remove_tags() -> None:
-    global FILE_COUNT
+def remove_tags(path: str, file_count: int) -> None:
+    '''Removing all possible MP3 tags'''
     curr_count = 0
-    for file in os.listdir(PATH):
+    for file in os.listdir(path):
         if file.endswith('.mp3'):
             curr_count += 1
-            file_path = os.path.join(PATH, file)
-            print(f'Removing tag from file {curr_count} out of {FILE_COUNT}')
+            file_path = os.path.join(path, file)
+            print(f'Removing tag from file {curr_count} out of {file_count}')
             try:
                 audio = ID3(file_path)
                 audio.delete()
@@ -60,15 +60,15 @@ def remove_tags() -> None:
                 continue
 
 
-def file_recreation() -> None:
-    global FILE_COUNT
+def file_recreation(path: str, file_count: int) -> None:
+    '''Modifying date properties by recreating files'''
     curr_count = 0
-    for file in os.listdir(PATH):
+    for file in os.listdir(path):
         if file.endswith('.mp3'):
             curr_count += 1
-            original_file_path = os.path.join(PATH, file)
-            new_file_path = os.path.join(PATH, file + ".bak")
-            print(f'Recreating file {curr_count} out of {FILE_COUNT}')
+            original_file_path = os.path.join(path, file)
+            new_file_path = os.path.join(path, file + ".bak")
+            print(f'Recreating file {curr_count} out of {file_count}')
             with open(original_file_path, 'rb') as original_file, open(new_file_path, 'wb') as new_file:
                 content = original_file.read()
                 new_file.write(content)
@@ -76,5 +76,6 @@ def file_recreation() -> None:
             os.rename(new_file_path, original_file_path)
 
 
-count_files()
+PATH = 'F://'
+FILE_COUNT = count_files(PATH)
 hello()
